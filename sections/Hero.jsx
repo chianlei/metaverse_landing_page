4417cd3be8c61dfa3b8d681716c76ca4bc822e5e
chian lei so <chianlei.so@atoz-software.tech>
2022-12-29 17:09:32 +0800

    first commit

---
+++ sections/Hero.jsx
@@ -0,0 +1,9 @@
+"use client";
+
+import { motion } from "framer-motion";
+import styles from "../styles";
+import { slideIn,staggerContainer, textVariant } from "../utils/motion";
+
+const Hero = () => <section className={`${styles.yPaddings} sm:pl-16 pl-6`}>Hiii</section>;
+
+export default Hero;
